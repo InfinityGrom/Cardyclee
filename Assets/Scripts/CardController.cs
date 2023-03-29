64d611993f1225212db35d8bb22914eb24f8c236
InfinityGrom <62838933+InfinityGrom@users.noreply.github.com>
2023-03-29 11:53:51 +0900

    Refined card system

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -7,6 +7,8 @@ using UnityEngine.UI;
 public class CardController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform parentToReturnTo = null;
+    public Transform placeholderParent = null;
+
 
     public Vector3 offset;
 
@@ -14,23 +16,24 @@ public class CardController : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     
     public Type typeOfCard = Type.FRIENDLY;
 
-    //GameObject placeholder = null;
+    public GameObject placeholder = null;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //placeholder = new GameObject();
-        //placeholder.transform.SetParent(this.transform.parent);
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
 
         parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
         this.transform.SetParent(this.transform.parent.parent);
 
-        //LayoutElement le = placeholder.AddComponent<LayoutElement>();
-        //le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
-        //le.preferredWidth = this. GetComponent<LayoutElement>().preferredWidth;
-        //le.flexibleHeight = 0;
-        //le.flexibleWidth = 0;
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.preferredWidth = this. GetComponent<LayoutElement>().preferredWidth;
+        le.flexibleHeight = 0;
+        le.flexibleWidth = 0;
 
-        //placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
         //This line fixes the mouse teleportation to the center of the card when clicked
         offset = Input.mousePosition-this.transform.position;
         
@@ -41,12 +44,38 @@ public class CardController : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
     public void OnDrag(PointerEventData eventData)
     {
         this.transform.position = Input.mousePosition-offset;
+
+        if (placeholder.transform.parent!= placeholderParent)
+        {
+            placeholder.transform.SetParent(placeholderParent);
+        }
+
+        int newSiblingIndex = placeholderParent.childCount;
+
+        for (int i=0; i< placeholderParent.childCount; i++)
+        {
+            if (this.transform.position.x< placeholderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+
+                if (placeholder.transform.GetSiblingIndex()< newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                    
+                }
+
+                break;
+            }
+        }
+
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         this.transform.SetParent(parentToReturnTo);
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
-        //Destroy(placeholder);
+        Destroy(placeholder);
     }
 }
--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -3,9 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public CardController.Type typeOfCard;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag==null) return;
+
+        CardController c = eventData.pointerDrag.GetComponent<CardController>();
+
+        if (c != null)
+        {
+            if (typeOfCard == c.typeOfCard)
+            {
+                c.placeholderParent = this.transform;
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null) return;
+
+        CardController c = eventData.pointerDrag.GetComponent<CardController>();
+
+        if ((c != null)&&(c.placeholderParent == this.transform))
+        {
+            if (typeOfCard == c.typeOfCard)
+            {
+                c.placeholderParent = c.parentToReturnTo;
+            }
+        }
+    }
     public void OnDrop(PointerEventData eventData)
     {
         CardController c = eventData.pointerDrag.GetComponent<CardController>();
@@ -17,4 +46,6 @@ public class DropZone : MonoBehaviour, IDropHandler
             }
         }
     }
+
+    
 }
