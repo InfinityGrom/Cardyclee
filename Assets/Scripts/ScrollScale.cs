c4347b32f5a44c49adf559d52bd34bb4889ca75c
InfinityGrom <62838933+InfinityGrom@users.noreply.github.com>
2023-04-12 12:17:10 +0900

    Unity update

---
+++ Assets/Scripts/ScrollScale.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollScale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RectTransform transform = GetComponent<RectTransform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
